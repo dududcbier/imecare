9e1cdf1b07101d450c4957210ba6a6f243a9e594
Eduardo Bier <dududcbier@gmail.com>
2015-11-27 18:22:45 -0200

    Mais testes em TestScheduleDao

--- workspace/fj21-jdbc/src/teste/TestScheduleDao.java
+++ workspace/fj21-jdbc/src/teste/TestScheduleDao.java
@@ -34,7 +34,60 @@ public class TestScheduleDao {
 		sched.setComment("Simpático");
 
 		insertSchedule(sched);
-		// assert statements
-		// assertEquals("Result must be -1 (SQL Exception)", -1, insertSchedule(sched));
+	}
+
+	@Test(expected=RuntimeException.class)
+	public void insertDoctorEmptyCPF() {
+
+		Schedule sched = new Schedule();
+		sched.setDoctorCpf("");
+		sched.setPatientCpf("10987654321");
+		sched.setDateTime(Calendar.getInstance());
+		sched.setType("refazer");
+		sched.setComment("Simpático");
+
+		insertSchedule(sched);
+	}
+
+	@Test
+	public void insertValidSchedule() {
+
+		Schedule sched = new Schedule();
+		sched.setDoctorCpf("12345678901");
+		sched.setPatientCpf("10987654321");
+		sched.setDateTime(Calendar.getInstance());
+		sched.setType("refazer");
+		sched.setComment("Simpático");
+
+		assertEquals("Must be able to insert", 0, insertSchedule(sched));
+	}
+
+	@Test(expected=RuntimeException.class) 
+	public void insertDuplicatedSchedule(){
+
+		Schedule sched = new Schedule();
+		sched.setDoctorCpf("12345678901");
+		sched.setPatientCpf("10987654321");
+		sched.setDateTime(Calendar.getInstance());
+		sched.setType("refazer");
+		sched.setComment("Simpático");
+
+		assertEquals("Must be able to insert", 0, insertSchedule(sched));
+		assertEquals("Must not be able to insert", -1, insertSchedule(sched));
+
+	}
+
+	@Test(expected=RuntimeException.class) 
+	public void insertNullDate(){
+
+		Schedule sched = new Schedule();
+		sched.setDoctorCpf("12345678901");
+		sched.setPatientCpf("10987654321");
+		sched.setDateTime(null);
+		sched.setType("refazer");
+		sched.setComment("Simpático");
+
+		assertEquals("Must not be able to insert", -1, insertSchedule(sched));
+
 	}
 } 
